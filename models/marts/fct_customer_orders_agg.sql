ad137d068538c2796a838546a1f40013888886b8
Isabelle Lodin <isabelle.lodin+1@gmail.com>
2025-09-02 12:38:47 +0000

    feature : changement de code

--- models/marts/fct_customer_orders_agg.sql
+++ models/marts/fct_customer_orders_agg.sql
@@ -12,20 +12,27 @@ cust as (
     c.city,
     c.state
   from {{ ref('stg_customers') }} c
+),
+agg as (
+  select
+    d.customer_id,
+    cust.customer_name,
+    cust.email,
+    cust.phone,
+    cust.city,
+    cust.state,
+    min(d.order_day)                          as first_order_date,
+    max(d.order_day)                          as last_order_date,
+    sum(d.orders_count)                       as orders_count,
+    sum(d.units_sold)                         as units_sold,
+    round(sum(d.revenue_net), 2)              as revenue_net,
+    case when sum(d.orders_count) > 1 then 1 else 0 end as is_recurrent
+  from daily d
+  left join cust on d.customer_id = cust.customer_id
+  group by 1,2,3,4,5,6
 )
+
 select
-  d.customer_id,
-  cust.customer_name,
-  cust.email,
-  cust.phone,
-  cust.city,
-  cust.state,
-  min(d.order_day)                          as first_order_date,
-  max(d.order_day)                          as last_order_date,
-  sum(d.orders_count)                       as orders_count,
-  sum(d.units_sold)                         as units_sold,
-  round(sum(d.revenue_net), 2)              as revenue_net,
-  case when sum(d.orders_count) > 1 then 1 else 0 end as is_recurrent
-from daily d
-left join cust on d.customer_id = cust.customer_id
-group by 1,2,3,4,5,6
+  *,
+  row_number() over (order by revenue_net desc) as customer_rank
+from agg
